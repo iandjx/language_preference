304cf33a70b2e5bee2588853a39f9c71571cb745
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-11-24 07:55:26 +0800

    Initial Commit

---
+++ app/src/main/java/io/techup/android/languagepreference/MainActivity.java
@@ -0,0 +1,13 @@
+package io.techup.android.languagepreference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
