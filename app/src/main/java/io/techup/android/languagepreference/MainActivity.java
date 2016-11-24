60b19b7fda4a6d182c5909a27702721bb79cc20a
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-11-24 08:09:38 +0800

    [WIP] Add alert dialog

--- app/src/main/java/io/techup/android/languagepreference/MainActivity.java
+++ app/src/main/java/io/techup/android/languagepreference/MainActivity.java
@@ -1,7 +1,10 @@
 package io.techup.android.languagepreference;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_language)
+                .setTitle("Are you sure?")
+                .setMessage("Do you definitely want to do this")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "its done", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
