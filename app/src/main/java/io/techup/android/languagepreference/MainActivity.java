a5ebdaf92a66dd2128559aa887f82c328c556add
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-11-24 11:56:51 +0800

    Set textview text to user selected language

--- app/src/main/java/io/techup/android/languagepreference/MainActivity.java
+++ app/src/main/java/io/techup/android/languagepreference/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView languageTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        languageTv = (TextView) findViewById(R.id.tv_language);
         new AlertDialog.Builder(this)
                 .setIcon(R.drawable.ic_language_black_24dp)
                 .setTitle("Choose a language")
@@ -19,10 +24,15 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("SPANISH", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), "its done", Toast.LENGTH_LONG).show();
+                        languageTv.setText("Spanish");
                     }
                 })
-                .setNegativeButton("ENGLISH", null)
+                .setNegativeButton("ENGLISH", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        languageTv.setText("English");
+                    }
+                })
                 .show();
     }
 }
