501acce6998ee9657d94120625c7a8dccf889f56
Ian de Jesus <ian.dejesus.md@gmail.com>
2016-11-24 13:04:53 +0800

    Display menu in main activity

--- app/src/main/java/io/techup/android/languagepreference/MainActivity.java
+++ app/src/main/java/io/techup/android/languagepreference/MainActivity.java
@@ -4,12 +4,45 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView languageTv;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.menu_english:
+                languageTv.setText("English");
+                return true;
+
+
+            case R.id.menu_spanish:
+                languageTv.setText("Spanish");
+                return true;
+            default:
+                return false;
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
